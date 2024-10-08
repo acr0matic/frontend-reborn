269f406f728769a8138fe5b0d178b02c9fd65bc7
acr0matic <work.acr0matic@gmail.com>
2024-10-08 15:14:43 +0300

    feat: Доработка SASS-архитектуры

--- src/js/global/var.js
+++ src/js/global/var.js
@@ -1,4 +1,11 @@
+const body = document.body;
+const html = document.documentElement;
+
 export const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+export const pageHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+
+
 export let scrollDirection;
 
 window.addEventListener('scroll', function handleScroll() {
