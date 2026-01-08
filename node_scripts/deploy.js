167f0d99a14bd0092ed85127450ce1275772d5a9
acr0matic <work.acr0matic@gmail.com>
2026-01-08 21:46:47 +0300

    feat: данные для фтп в .env файле

--- node_scripts/deploy.js
+++ node_scripts/deploy.js
@@ -1,4 +1,10 @@
 const ftp = require("basic-ftp")
+const dotenv = require("dotenv")
+
+dotenv.config()
+
+const secureValues = new Set(["true", "1", "yes", "on"])
+const secureString = (process.env.FTP_SECURE || "true").toLowerCase()
 
 async function deploy() {
   const client = new ftp.Client();
@@ -8,10 +14,10 @@ async function deploy() {
   try {
     // Здесь прописываем данные от FTP
     await client.access({
-      host: "",
-      user: "",
-      password: "",
-      secure: true
+      host: process.env.FTP_HOST,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD,
+      secure: secureValues.has(secureString)
     });
 
     await client.ensureDir("dist/") // Путь к папке на удаленном сервере
